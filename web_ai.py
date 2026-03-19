2e574daf93f645ef1f9b990d86929f92bfd66543
ziwwalfatihzaw-create <ziww.alfatihzaw@gmail.com>
2026-03-19 22:53:50 +0700

    Update app to use environment variable for port

--- web_ai.py
+++ web_ai.py
@@ -42,4 +42,6 @@ def chat():
         return jsonify({"jawaban": "Belum tahu 😅"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
